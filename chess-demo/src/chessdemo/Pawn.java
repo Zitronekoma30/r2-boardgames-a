00ff588b75fff1262f1a076075d254fa81234e12
Leon A. <69749337+Zitronekoma30@users.noreply.github.com>
2024-12-01 14:00:13 +0100

    add building pawn movement rules in chess demo #7

--- chess-demo/src/chessdemo/Pawn.java
+++ chess-demo/src/chessdemo/Pawn.java
@@ -1,9 +1,29 @@
 package chessdemo;
 
 import com.boardgame.core.GamePiece;
+import com.boardgame.core.model.MoveCountLessMovementRule;
+import com.boardgame.core.model.MovementRule;
+import com.boardgame.core.model.PositionMovementRule;
 
 public class Pawn extends GamePiece{
-    public Pawn(){
+    public Pawn(boolean white){
         setSprite("pawn.png");
+
+        int up;
+
+        if (white) up = -1;
+        else up = 1;
+
+        MovementRule firstMoveMR =
+                new MoveCountLessMovementRule(
+                        new PositionMovementRule(null, new int[][]{{0, up}, {0, up*2}}),
+                        1);
+
+        MovementRule upMR = new PositionMovementRule(null, new int[][]{{0, up}});
+        MovementRule downMR = new PositionMovementRule(null, new int[][]{{0, -up}});
+
+        addMovementRule(firstMoveMR);
+        addMovementRule(downMR);
+        addMovementRule(upMR);
     }
 }
