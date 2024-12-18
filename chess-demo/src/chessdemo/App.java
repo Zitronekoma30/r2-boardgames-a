d08e784bc2f723edb206d24089a2730bcb0d8c73
Leon A. <69749337+Zitronekoma30@users.noreply.github.com>
2024-12-18 13:49:23 +0100

    fix chess demo piece arrangement #14

--- chess-demo/src/chessdemo/App.java
+++ chess-demo/src/chessdemo/App.java
@@ -33,16 +33,16 @@ public class App {
     }
 
     private static void addPieces(Player player, boolean white, GameBoard board) {
+        int mainRow = white ? 7 : 0; // y:0 is black's home row, y:7 is white's home row
         int pawnRow = white ? 6 : 1;
-        int mainRow = white ? 7 : 0;
         int queenColumn = 3;
         int kingColumn = 4;
 
         // Add pawns
-        for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) { // Iterate through columns for pawns
             Pawn pawn = new Pawn(white);
             player.addPiece(pawn);
-            board.placePiece(pawnRow, y, pawn);
+            board.placePiece(x, pawnRow, pawn); // Place pawns on the pawnRow
         }
 
         // Add rooks
@@ -50,34 +50,35 @@ public class App {
         Rook rook2 = new Rook(white);
         player.addPiece(rook1);
         player.addPiece(rook2);
-        board.placePiece(mainRow, 0, rook1);
-        board.placePiece(mainRow, 7, rook2);
+        board.placePiece(0, mainRow, rook1); // Place rook at column 0
+        board.placePiece(7, mainRow, rook2); // Place rook at column 7
 
         // Add knights
         Knight knight1 = new Knight(white);
         Knight knight2 = new Knight(white);
         player.addPiece(knight1);
         player.addPiece(knight2);
-        board.placePiece(mainRow, 1, knight1);
-        board.placePiece(mainRow, 6, knight2);
+        board.placePiece(1, mainRow, knight1); // Place knight at column 1
+        board.placePiece(6, mainRow, knight2); // Place knight at column 6
 
         // Add bishops
         Bishop bishop1 = new Bishop(white);
         Bishop bishop2 = new Bishop(white);
         player.addPiece(bishop1);
         player.addPiece(bishop2);
-        board.placePiece(mainRow, 2, bishop1);
-        board.placePiece(mainRow, 5, bishop2);
+        board.placePiece(2, mainRow, bishop1); // Place bishop at column 2
+        board.placePiece(5, mainRow, bishop2); // Place bishop at column 5
 
         // Add queen
         Queen queen = new Queen(white);
         player.addPiece(queen);
-        board.placePiece(mainRow, queenColumn, queen);
+        board.placePiece(queenColumn, mainRow, queen); // Place queen at column 3
 
         // Add king
         King king = new King(white, new GamePiece[]{});
         player.addPiece(king);
-        board.placePiece(mainRow, kingColumn, king);
+        board.placePiece(kingColumn, mainRow, king); // Place king at column 4
     }
+
 }
 
