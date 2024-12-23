f9952a4e37fcc58006b671065362d69876c0c3db
Leon A. <69749337+Zitronekoma30@users.noreply.github.com>
2024-12-23 11:59:37 +0100

    implement rook movement #14

--- chess-demo/src/chessdemo/pieces/Rook.java
+++ chess-demo/src/chessdemo/pieces/Rook.java
@@ -2,6 +2,8 @@ package chessdemo.pieces;
 
 import com.boardgame.core.Player;
 import com.boardgame.core.model.move.ContinuousMovementRule;
+import com.boardgame.core.model.move.MovementRule;
+import com.boardgame.core.model.move.PieceOwnerBlacklistMovementRule;
 
 public class Rook extends ChessPiece{
     public Rook(Player player, Player opponent) {
@@ -10,9 +12,10 @@ public class Rook extends ChessPiece{
 
     @Override
     protected void setupMovementRules(int up) {
-        addMovementRule(new ContinuousMovementRule(null, new int[] {0, 1}, 8));
-        addMovementRule(new ContinuousMovementRule(null, new int[] {0, -1}, 8));
-        addMovementRule(new ContinuousMovementRule(null, new int[] {1, 0}, 8));
-        addMovementRule(new ContinuousMovementRule(null, new int[] {-1, 0}, 8));
+        MovementRule notOwn = new PieceOwnerBlacklistMovementRule(null, getPlayer());
+        addMovementRule(new ContinuousMovementRule(notOwn, new int[] {0, 1}, 8));
+        addMovementRule(new ContinuousMovementRule(notOwn, new int[] {0, -1}, 8));
+        addMovementRule(new ContinuousMovementRule(notOwn, new int[] {1, 0}, 8));
+        addMovementRule(new ContinuousMovementRule(notOwn, new int[] {-1, 0}, 8));
     }
 }
