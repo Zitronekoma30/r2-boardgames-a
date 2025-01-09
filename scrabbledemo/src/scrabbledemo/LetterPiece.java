51ddd2ab68edb7da658e4827caa5feac89d60790
Leon A. <69749337+Zitronekoma30@users.noreply.github.com>
2025-01-09 18:07:32 +0100

    add checking adjacency when placing tiles from hand #22

--- scrabbledemo/src/scrabbledemo/LetterPiece.java
+++ scrabbledemo/src/scrabbledemo/LetterPiece.java
@@ -6,11 +6,21 @@ import com.boardgame.core.Player;
 public class LetterPiece extends GamePiece {
     private Player player;
     private Character letter;
+    private boolean newlyPlaced;
 
     public LetterPiece(Player player, Character letter){
         this.player = player;
         this.letter = letter;
+        this.newlyPlaced = false;
 
         setSprite("letter_"+letter.toString().toUpperCase()+".png");
+    }
+
+    public boolean getNewlyPlaced() {
+        return newlyPlaced;
+    }
+
+    public void setNewlyPlaced(boolean newlyPlaced){
+        this.newlyPlaced = newlyPlaced;
     }
 }
--- scrabbledemo/src/scrabbledemo/LetterPlayValidator.java
+++ scrabbledemo/src/scrabbledemo/LetterPlayValidator.java
@@ -4,12 +4,44 @@ import com.boardgame.core.GameBoard;
 import com.boardgame.core.model.hand.Action;
 import com.boardgame.core.model.hand.PlayValidator;
 
+import java.util.*;
+
 public class LetterPlayValidator extends PlayValidator {
 
     @Override
     public boolean validate(Action[] actions, GameBoard gameBoard) {
-        // TODO: Check if all destinations are adjacent (non diagonal)
-        // TODO: Mark all pieces as "new" so the board can recognize them and roll back if needed
-        return false;
+        // Check if all destinations are adjacent (non diagonal) (simple BFS)
+        var positions = Arrays.stream(actions).map(action -> new int[]{action.to().getX(), action.to().getY()}).toArray(int[][]::new);
+        if (positions.length == 0) return false;
+
+        Set<String> visited = new HashSet<>();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(positions[0]);
+        visited.add(Arrays.toString(positions[0]));
+
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            for (int[] dir : directions) {
+                int[] neighbor = {pos[0] + dir[0], pos[1] + dir[1]};
+                if (Arrays.stream(positions).anyMatch(p -> Arrays.equals(p, neighbor)) && !visited.contains(Arrays.toString(neighbor))) {
+                    queue.add(neighbor);
+                    visited.add(Arrays.toString(neighbor));
+                    count++;
+                }
+            }
+        }
+
+        if (count != positions.length) return false;
+
+        // Mark all pieces as "new" so the board can recognize them and roll back if needed
+        for (Action a : actions){
+            LetterPiece piece = (LetterPiece) a.piece();
+            piece.setNewlyPlaced(true);
+        }
+
+        return true;
     }
 }
