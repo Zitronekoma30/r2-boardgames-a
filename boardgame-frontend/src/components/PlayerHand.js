b68a92899c64d9bb5f55b7e3af58b9f827daf479
AdreasKoessler <73241802+AdreasKoessler@users.noreply.github.com>
2025-01-30 00:38:08 +0100

    Update PlayerHand.js for visual QoL functionality

--- boardgame-frontend/src/components/PlayerHand.js
+++ boardgame-frontend/src/components/PlayerHand.js
@@ -4,6 +4,7 @@ import SubmitHand from './SubmitHand';
 
 const PlayerHand = () => {
   const [handData, setHandData] = useState([]);
+  const [isHidden, setIsHidden] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,6 +12,7 @@ const PlayerHand = () => {
         try {
           const data = await fetchHandData();
           setHandData(data);
+          setIsHidden(false);
         } catch (error) {
           console.error('Error fetching hand data:', error);
         }
@@ -34,7 +36,7 @@ const PlayerHand = () => {
   );
 
   return (
-    <div className="PlayerHand">
+    <div className={`PlayerHand ${isHidden ? "hide" : ""}`}>
       <h2>Your Hand</h2>
       <div className="hand">
         {handData.map((piece, index) => renderHandPiece(piece, index))}
